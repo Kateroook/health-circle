4908c21fde326ae0aa7054ea13d796a66bd2f864
ngnsr <vladislavrisengin895@gmail.com>
2025-11-22 16:40:06 +0200

    feat: improve auth

--- apps/core-api/src/users/users.controller.ts
+++ apps/core-api/src/users/users.controller.ts
@@ -1,27 +1,28 @@
-import { Body, Controller, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
-import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UUIdParamDto } from 'src/common/dto/uuid-param.dto';
 import { UserEntity } from 'src/common/entities/user.entity';
 
+import { AuthStrategies } from 'src/common/enums/auth-strategies';
+import { UserJwtAccessGuard } from 'src/common/guards/user-jwt-access.guard';
 import type { AuthRequest } from 'src/common/types/auth-request';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ModifyUserDto } from './dto/modify-user.dto';
 import { UsersService } from './users.service';
 
-// TODO: add access checks
 @ApiTags('Users CRUD API')
 @Controller('users')
-// @ApiBearerAuth(AuthStrategies.userJwtAccess)
-// @UseGuards(UserJwtAccessGuard)
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
   @Get(':id')
+  @ApiBearerAuth(AuthStrategies.userJwtAccess)
+  @UseGuards(UserJwtAccessGuard)
   @ApiOperation({ summary: 'Retrieve user by id' })
   @ApiOkResponse({ type: UserEntity, description: 'User found' })
   @ApiNotFoundResponse({ description: 'User not found' })
-  async getOne(@Param() params: UUIdParamDto) {
-    return this.service.getOne(params.id);
+  async getOne(@Param() params: UUIdParamDto, @Req() req: AuthRequest) {
+    return this.service.getOne(params.id, req.user);
   }
 
   @Post()
@@ -31,17 +32,21 @@ export class UsersController {
     description: 'User created successfully',
   })
   async create(@Body() body: CreateUserDto, @Req() req: AuthRequest) {
-    return this.service.save(body, req.metadata, true);
+    return this.service.save(body, req.metadata, true, req.user);
   }
 
   @Put()
+  @ApiBearerAuth(AuthStrategies.userJwtAccess)
+  @UseGuards(UserJwtAccessGuard)
   @ApiOperation({ summary: 'Modify user with relations' })
   @ApiOkResponse({ type: UserEntity, description: 'User updated successfully' })
   async modify(@Body() body: ModifyUserDto, @Req() req: AuthRequest) {
-    return this.service.save(body, req.metadata, false);
+    return this.service.save(body, req.metadata, false, req.user);
   }
 
   @Patch(':id/reset-password')
+  @ApiBearerAuth(AuthStrategies.userJwtAccess)
+  @UseGuards(UserJwtAccessGuard)
   @ApiOperation({ summary: 'Reset user password' })
   @ApiOkResponse({ description: 'Password reset successfully' })
   async resetPassword(@Param() params: UUIdParamDto, @Req() req: AuthRequest) {
--- apps/core-api/src/users/users.service.ts
+++ apps/core-api/src/users/users.service.ts
@@ -7,8 +7,10 @@ import { SetupPasswordReasons } from 'src/confirmations/enums/setup-password-rea
 import { UserActivitiesService } from 'src/user-activities/user-activities.service';
 import { Repository } from 'typeorm';
 
+import { UserProfileDto } from 'src/common/dto/user-profile.dto';
 import { UserPasswordEntity } from 'src/common/entities/user-password.entity';
 import { UserActivityTypes } from 'src/common/enums/user-activity-types';
+import { ensureSameUser } from 'src/common/helpers/ensure-same-user.util';
 import { RequestMetadata } from 'src/common/types/request-metadata';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ModifyUserDto } from './dto/modify-user.dto';
@@ -29,25 +31,29 @@ export class UsersService {
     return this.repository.createQueryBuilder('users');
   }
 
-  public async getOne(id: string) {
-    const user = await this.getOneQueryBuilder().where('users.id = :id', { id }).getOne();
-    if (!user) throw new NotFoundException(`Користувача з id = ${id} не знайдено`);
-    return user;
+  public async getOne(id: string, user: UserProfileDto): Promise<UserEntity> {
+    ensureSameUser(id, user.id);
+    const targetUser = await this.getOneQueryBuilder().where('users.id = :id', { id }).getOne();
+    if (!targetUser) throw new NotFoundException(`Користувача з id = ${id} не знайдено`);
+    return targetUser;
   }
 
-  public async save(item: CreateUserDto | ModifyUserDto, metadata: RequestMetadata, isNew = false): Promise<UserEntity> {
-    // todo: add auth check
+  public async save(
+    item: CreateUserDto | ModifyUserDto,
+    metadata: RequestMetadata,
+    isNew = false,
+    user: UserProfileDto,
+  ): Promise<UserEntity> {
     if (!isNew && 'id' in item) {
-      const user = await this.repository.findOneBy({ id: item.id });
-      if (!user) throw new NotFoundException(`Користувача з id = ${item.id} не знайдено`);
+      ensureSameUser(item.id, user.id);
+      const exists = await this.repository.existsBy({ id: item.id });
+      if (!exists) throw new NotFoundException(`Користувача з id = ${item.id} не знайдено`);
     }
-    const user = await this.repository.save(item);
+    const saved = await this.repository.save(item);
     const userActivityType = isNew ? UserActivityTypes.createUser : UserActivityTypes.modifyUser;
-    if (isNew) {
-      await this.confirmationsService.setupPasswordCode(user.email, user.id, SetupPasswordReasons.setup);
-    }
+    if (isNew) await this.confirmationsService.setupPasswordCode(saved.email, user.id, SetupPasswordReasons.setup);
     await this.userActivitiesService.logActivity(userActivityType, metadata, { userId: user.id });
-    return user;
+    return saved;
   }
 
   async resetPassword(userId: string, metadata: RequestMetadata) {
