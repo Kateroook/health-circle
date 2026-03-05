20d320c580644f82cd8df5448b998f706010cb18
ngnsr <vrisenhin@huspi.com>
2026-03-06 01:56:32 +0200

    fix: ios build

--- apps/client/plugins/withPodfileAllowNonModularIncludes.js
+++ apps/client/plugins/withPodfileAllowNonModularIncludes.js
@@ -27,10 +27,17 @@ function withPodfileAllowNonModularIncludes(config) {
     installer.pods_project.targets.each do |target|
       if target.name.start_with?('RNFB')
         target.build_configurations.each do |config|
-          config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
           config.build_settings['DEFINES_MODULE'] = 'YES'
           config.build_settings['GCC_C_LANGUAGE_STANDARD'] = 'gnu99'
-          config.build_settings['HEADER_SEARCH_PATHS'] = '$(inherited) "\${PODS_ROOT}/Headers/Public/RNFBApp" "\${PODS_CONFIGURATION_BUILD_DIR}/RNFBApp/RNFBApp.framework/Headers"'
+          config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+          config.build_settings['CLANG_ENABLE_MODULES'] = 'YES'
+          config.build_settings['HEADER_SEARCH_PATHS'] = [
+            '$(inherited)',
+            '"${PODS_ROOT}/Headers/Public/RNFBApp"',
+            '"${PODS_CONFIGURATION_BUILD_DIR}/RNFBApp/RNFBApp.framework/Headers"',
+            '"${PODS_ROOT}/RNFBApp"',
+            '"$(SRCROOT)/../node_modules/@react-native-firebase/app/ios/RNFBApp"'
+          ].join(' ')
           config.build_settings['OTHER_LDFLAGS'] = '$(inherited) -framework "RNFBApp"'
         end
       end
