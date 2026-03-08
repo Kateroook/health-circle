7496d70a615bf727a0626e577f1be5cfb394555a
ngnsr <vrisenhin@huspi.com>
2026-03-08 13:41:50 +0200

    feat: roll call add migrations

--- apps/core-api/src/common/migrations/index.ts
+++ apps/core-api/src/common/migrations/index.ts
@@ -21,6 +21,7 @@ import { CascadeDeleteAndRelaxLogConstraints1772048762122 } from './177204876212
 import { MakeMiddleNameOptional1772561853196 } from './1772561853196-MakeMiddleNameOptional';
 import { AlterUserEntity1772743614997 } from './1772743614997-alter-user-entity';
 import { LowercaseExistingEmails1772743615000 } from './1772743615000-LowercaseExistingEmails';
+import { AlterUserEntity1772969940227 } from './1772969940227-alter-user-entity';
 
 export const migrations = [
   CreateEntities1762501994652,
@@ -46,4 +47,5 @@ export const migrations = [
   MakeMiddleNameOptional1772561853196,
   AlterUserEntity1772743614997,
   LowercaseExistingEmails1772743615000,
+  AlterUserEntity1772969940227,
 ];
