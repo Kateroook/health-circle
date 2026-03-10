a7f0e2b8b5cedc45f6a9d23792fa419fc2afc427
ngnsr <vrisenhin@huspi.com>
2026-03-10 11:23:19 +0200

    feat: improve sync

--- apps/client/src/app/(tabs)/Dashboard.tsx
+++ apps/client/src/app/(tabs)/Dashboard.tsx
@@ -327,11 +327,11 @@ export default function DashboardScreen() {
     }
   };
 
+  useSyncSignal(fetchGroups);
+
   useFocusEffect(
     useCallback(() => {
       fetchGroups();
-      const interval = setInterval(fetchGroups, 5000);
-      return () => clearInterval(interval);
     }, []),
   );
 
