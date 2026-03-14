d9515b7db3781c5abe4bcbdc0d5bcb1e9a78e4c2
ngnsr <vrisenhin@huspi.com>
2026-03-14 18:57:49 +0200

    fix: migrations

--- apps/core-api/src/common/migrations/index.ts
+++ apps/core-api/src/common/migrations/index.ts
@@ -1,4 +1,5 @@
 import { Baseline1773479991424 } from './1773479991424-baseline';
 import { AddLocationColumns1773502191466 } from './1773502191466-add-location-columns';
+import { AddJoinedAtToGroupMembers1773516929000 } from './1773516929000-add-joined-at-to-group-members';
 
-export const migrations = [Baseline1773479991424, AddLocationColumns1773502191466];
+export const migrations = [Baseline1773479991424, AddLocationColumns1773502191466, AddJoinedAtToGroupMembers1773516929000];
