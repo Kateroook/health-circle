2b9fe0c48cf70077e2ff6a3bc520f17fe0a9c3b0
8ctag8ne <nazaryagotin@gmail.com>
2026-03-18 02:10:11 +0200

    add auth/forgot-password tests

--- apps/test-framework/src/api/tests/auth/auth.change-password.spec.ts
+++ apps/test-framework/src/api/tests/auth/auth.change-password.spec.ts
@@ -1,9 +1,3 @@
-// | AUTH-029 | Невірний поточний пароль | Авторизований, некоректний `oldPassword` | 401 |
-// | AUTH-030 | Паролі не збігаються | Авторизований, `newPassword` ≠ `confirmNewPassword` | 400 |
-// | AUTH-031 | Новий пароль не відповідає вимогам | Авторизований, короткий `newPassword` | 400 |
-// | AUTH-032 | Запит без авторизації | Без токена | 401 |
-// | AUTH-033 | Старий пароль не працює після зміни | Виконано change-password | 401 при логіні зі старим паролем |
-// | AUTH-034 | Новий пароль працює після зміни | Виконано change-password | 200 при логіні з новим паролем |
 import { UserEntity } from '../../../core/types/entites/user-interface';
 import { utils } from '../../../utils/utils';
 import { expect, test } from '../../fixtures/api-fixture';
@@ -28,5 +22,94 @@ test.describe('api/auth/change-password tests', async () => {
     });
 
     expect(changePassword.response).toHaveStatus2xx();
+  });
+  // | AUTH-029 | Невірний поточний пароль | Авторизований, некоректний `oldPassword` | 401 |
+  test('[AUTH-029] Wrong old password for password change', async ({ api, spawnUser }) => {
+    const newPassword = utils.random.password(12);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password+'1',
+        newPassword: newPassword,
+        confirmNewPassword: newPassword,
+    });
+
+    expect(changePassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-030 | Паролі не збігаються | Авторизований, `newPassword` ≠ `confirmNewPassword` | 400 |
+  test('[AUTH-030] Password mismatch in password change', async ({ api, spawnUser }) => {
+    const newPassword = utils.random.password(12);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password,
+        newPassword: newPassword,
+        confirmNewPassword: newPassword+'2',
+    });
+
+    expect(changePassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-031 | Новий пароль не відповідає вимогам | Авторизований, короткий `newPassword` | 400 |
+  test('[AUTH-031] Short new password in password change', async ({ api, spawnUser }) => {
+    const newPassword = utils.random.password(11);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password,
+        newPassword: newPassword,
+        confirmNewPassword: newPassword,
+    });
+
+    expect(changePassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-032 | Запит без авторизації | Без токена | 401 |
+  test('[AUTH-032] Password change without access token', async ({ api, spawnUser }) => {
+    const logout = await api.auth.logout();
+    expect(logout.response).toHaveStatus2xx();
+
+    const newPassword = utils.random.password(12);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password,
+        newPassword: newPassword,
+        confirmNewPassword: newPassword,
+    });
+
+    expect(changePassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-033 | Старий пароль не працює після зміни | Виконано change-password | 401 при логіні зі старим паролем |
+  test("[AUTH-033] Old password doesn't work after successfull password change", async ({ api, spawnUser }) => {
+    const newPassword = utils.random.password(12);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password,
+        newPassword: newPassword,
+        confirmNewPassword: newPassword,
+    });
+
+    const logout = await api.auth.logout();
+    expect(logout.response).toHaveStatus2xx();
+
+    const login = await api.auth.login({
+        identifier: user.email,
+        password: user.password,
+    })
+
+    expect(login.response).toHaveStatus4xx();
+    expect(api.getContext().accessToken).toBeUndefined();
+    expect(api.getContext().refreshToken).toBeUndefined();
+  });
+  // | AUTH-034 | Новий пароль працює після зміни | Виконано change-password | 200 при логіні з новим паролем |
+  test('[AUTH-034] New password works after successfull password change', async ({ api, spawnUser }) => {
+    const newPassword = utils.random.password(12);
+    const changePassword = await api.auth.changePassword({
+        oldPassword: user.password,
+        newPassword: newPassword,
+        confirmNewPassword: newPassword,
+    });
+
+    expect(changePassword.response).toHaveStatus2xx();
+
+    const logout = await api.auth.logout();
+    expect(logout.response).toHaveStatus2xx();
+
+    const login = await api.auth.login({
+        identifier: user.email,
+        password: newPassword,
+    })
+
+    expect(login.response).toHaveStatus2xx();
   });
 });
--- apps/test-framework/src/api/tests/auth/auth.forgot-password.spec.ts
+++ apps/test-framework/src/api/tests/auth/auth.forgot-password.spec.ts
@@ -1,6 +1,47 @@
 // | ID | Назва | Вхідні дані | Очікуваний результат |
 // |---|---|---|---|
-// | AUTH-035 | Запит на скидання для існуючого email | Зареєстрований email | 200 |
-// | AUTH-036 | Запит на скидання для незареєстрованого email | Випадковий email | 200 (безпечна відповідь, не розкриває існування акаунту) |
-// | AUTH-037 | Некоректний формат email | `notanemail` | 400 |
-// | AUTH-038 | Порожній email | `""` | 400 |
+
+
+
+import { UserFactory } from '../../../core/data/factories/user-factory';
+import { UserEntity } from '../../../core/types/entites/user-interface';
+import { utils } from '../../../utils/utils';
+import { expect, test } from '../../fixtures/api-fixture';
+
+test.describe('api/auth/change-password tests', async () => {
+  // | AUTH-035 | Запит на скидання для існуючого email | Зареєстрований email | 200 |
+  test('[AUTH-035] Forgot password request for existing email', async ({ api, spawnUser, confirmationCodeRepository }) => {
+    const user = await spawnUser();
+    const codeCountBefore = (await confirmationCodeRepository.findBy({userId: user.id})).length;
+    const forgotPassword = await api.auth.forgotPassword({
+        email: user.email,
+    });
+    
+    expect(forgotPassword.response).toHaveStatus2xx();
+
+    const codeCountAfter = (await confirmationCodeRepository.findBy({userId: user.id})).length;
+    expect(codeCountAfter - codeCountBefore).toEqual(1);
+  });
+  // | AUTH-036 | Запит на скидання для незареєстрованого email | Випадковий email | 400 |
+  test('[AUTH-036] Forgot password request for not registered email', async ({ api }) => {
+    const user = UserFactory.createUserForTest({testId: 'auth-36'});
+    const forgotPassword = await api.auth.forgotPassword({
+        email: user.email,
+    });
+    expect(forgotPassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-037 | Некоректний формат email | `notanemail` | 400 |
+  test('[AUTH-037] Forgot password request for incorrect email', async ({ api }) => {
+    const forgotPassword = await api.auth.forgotPassword({
+        email: 'notanemail',
+    });
+    expect(forgotPassword.response).toHaveStatus4xx();
+  });
+  // | AUTH-038 | Порожній email | `""` | 400 |
+  test('[AUTH-038] Forgot password request for an empty email', async ({ api }) => {
+    const forgotPassword = await api.auth.forgotPassword({
+        email: '',
+    });
+    expect(forgotPassword.response).toHaveStatus4xx();
+  });
+});
