28607bc67dc786c504d4f38815df9071b3fe0112
8ctag8ne <nazaryagotin@gmail.com>
2026-03-18 20:31:06 +0200

    add registration code tests

--- apps/test-framework/src/api/tests/auth/auth.resend-reg-code.spec.ts
+++ apps/test-framework/src/api/tests/auth/auth.resend-reg-code.spec.ts
@@ -1,2 +1,40 @@
-// | AUTH-045 | Успішна повторна відправка коду | Виконано createUser, токен не встановлений | 200 |
-// | AUTH-046 | Запит без авторизації | Без токена | Перевірити поведінку (може бути 401 або окремий flow) |
+import { UserFactory } from "../../../core/data/factories/user-factory";
+import { expect, test } from "../../fixtures/api-fixture";
+test.describe('api/auth/refresh tests', async () => {
+    // | AUTH-049 | Успішна повторна відправка коду | Виконано createUser, токен не встановлений | 200 |
+    test('[AUTH-049] Successful code resend', async ({ api, confirmationCodeRepository }) => {
+        let user = UserFactory.createRandomUser();
+        const postUser = await api.users.createUser({
+            email: user.email,
+            phone: user.phone,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        });
+        expect(postUser.response).toHaveStatus2xx();
+        user.id = postUser.data.id;
+
+        const codeCountBefore = (await confirmationCodeRepository.findBy({userId: user.id})).length;
+
+
+        const resendCode = await api.auth.resendRegistrationCode({
+            email: user.email,
+        });
+
+        expect(resendCode.response).toHaveStatus2xx();
+
+        const codeCountAfter = (await confirmationCodeRepository.findBy({userId: user.id})).length;
+
+        expect(codeCountAfter - codeCountBefore).toEqual(1);
+    });
+
+    // | AUTH-050 | Запит для незареєстрованого емейлу | Без токена | 401 |
+    test('[AUTH-050] Resend registration code for non-registered email', async ({ api }) => {
+        let user = UserFactory.createRandomUser();
+
+        const resendCode = await api.auth.resendRegistrationCode({
+            email: user.email,
+        });
+
+        expect(resendCode.response).toHaveStatus4xx();
+    });
+})
