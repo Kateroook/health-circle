f2edf1016c55c847259aeea90ad557a56d7deec4
8ctag8ne <nazaryagotin@gmail.com>
2026-04-07 03:15:33 +0300

    feat: add deep link basic test

--- apps/test-framework/src/mobile/tests/basic-test.spec.ts
+++ apps/test-framework/src/mobile/tests/basic-test.spec.ts
@@ -1,27 +1,25 @@
 import { $, expect as wdioExpect } from '@wdio/globals';
+import LoginScreen from '../screens/LoginScreen';
 
-describe('Test basic flow', () => {
+describe('Basic tests to verify mobile part integrity', () => {
   it('should navigate to registration page and verify instructions text', async () => {
-    // 1. Знаходимо кнопку "Зареєструватися" за описом (content-desc / accessibilityLabel)
-    // Використовуємо стратегію 'android=' для нативних селекторів UiSelector
     const registerButton = await $('android=new UiSelector().description("Зареєструватися")');
 
-    // Чекаємо, поки кнопка з'явиться (на випадок довгого завантаження Splash screen)
     await registerButton.waitForDisplayed({ timeout: 10000 });
 
-    // 2. Натискаємо на кнопку
     await registerButton.click();
 
-    // 3. Знаходимо текст на новій сторінці за точним збігом тексту
     const instructionText = await $('android=new UiSelector().text("Введи номер телефону та електронну пошту")');
     const actualText = await instructionText.getText();
-    console.log(`--- Знайдений текст на екрані: [${actualText}] ---`);
+    console.log(`--- Found text on screen: [${actualText}] ---`);
 
-    // 4. Перевіряємо, що текст відображається на екрані
-    // WDIO автоматично почекає появи елемента перед асертом
     await wdioExpect(instructionText).toBeDisplayed();
 
-    // Додатково можна перевірити, чи текст саме такий, як ми очікуємо
     wdioExpect(actualText).toBe('Введи номер телефону та електронну пошту');
+  });
+
+  it.only('Deep link navigation to login page', async () => {
+    await LoginScreen.openDirectly();
+    await LoginScreen.waitForIsShown();
   });
 });
