6cb8bd2185c0aa50e98222e863c75ea91841e80c
Repetiy Yulia <repetiy0905@gmail.com>
2026-04-22 23:55:50 +0300

    fix: fix wdio.conf.ts

--- apps/test-framework/src/mobile/wdio.conf.ts
+++ apps/test-framework/src/mobile/wdio.conf.ts
@@ -1,8 +1,8 @@
 import { browser } from '@wdio/globals';
+import path from 'path';
 import { BackendProvider } from '../core/backend-provider';
 
 export const config: WebdriverIO.Config = {
-  maxInstances: 1,
   //Where to find tests
   specs: ['./tests/**/*.spec.ts'],
   protocol: 'http',
@@ -27,15 +27,14 @@ export const config: WebdriverIO.Config = {
   //Platform settings
   capabilities: [
     {
-      'appium:noReset': true,
-      'appium:fullReset': false,
+      'appium:noReset': false,
       'appium:shouldTerminateApp': true,
-      'appium:enforceAppInstall': false,
+      'appium:enforceAppInstall': true,
       'appium:platformName': 'Android',
       'appium:automationName': 'UiAutomator2',
       'appium:platformVersion': process.env.APPIUM__PLATFORM_VERSION!,
       'appium:deviceName': process.env.APPIUM__DEVICE_NAME!,
-      'appium:app': 'C:/apps/health-circle-dev-63-android.apk',
+      'appium:app': path.join(process.cwd(), '../client/android/app/build/outputs/apk/release/app-release.apk'),
       'appium:autoGrantPermissions': true,
       'appium:newCommandTimeout': 300,
       'appium:udid': process.env.APPIUM__DEVICE_NAME!,
