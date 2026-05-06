c5f434fdc3ad9609f9db86275fe61bde1905b6f2
8ctag8ne <nazaryagotin@gmail.com>
2026-05-06 03:10:14 +0300

    fix: [HC-98] test, remove unneccessary timeouts

--- apps/test-framework/src/mobile/tests/notifications/background.spec.ts
+++ apps/test-framework/src/mobile/tests/notifications/background.spec.ts
@@ -1,122 +1,129 @@
 import { ApiClientFactory } from '@core/api/api-client-factory';
-import { AlertRegions } from '@core/data/regions';
+import { GroupFactory } from '@core/data/factories/group-factory';
+import { AlertRegion, AlertRegions } from '@core/data/regions';
+import { GroupEntity } from '@core/types/entites/group-interface';
 import { UserEntity } from '@core/types/entites/user-interface';
 import { NotificationHelper } from 'src/mobile/helpers/notification-helper';
 import dashboardScreen from 'src/mobile/screens/dashboard-screen';
 import loginScreen from 'src/mobile/screens/login-screen';
+import { utils } from 'src/utils/utils';
 import { NotificationType } from '../../../../../core-api/src/notifications/notification-types';
 
-// describe('Background Group Notifications', () => {
-//     let user1: UserEntity;
-//     let user2: UserEntity;
-//     let api1: ApiClientFactory;
-//     let api2: ApiClientFactory;
-//     let group: GroupEntity;
+describe('Background Group Notifications', () => {
+  let user1: UserEntity;
+  let user2: UserEntity;
+  let api1: ApiClientFactory;
+  let api2: ApiClientFactory;
+  let group: GroupEntity;
 
-//     beforeEach(async () => {
-//         await NotificationHelper.clearAll();
-//         await NotificationHelper.close();
-//         user1 = await browser.backend.spawnUser();
-//         user2 = await browser.backend.spawnUser();
-//         group = GroupFactory.createGroupWithSpecificOwner(user1);
-//         group.name = `BG notification test: ${utils.random.shortId()}`;
-//         api1 = browser.backend.api;
-//         api2 = await browser.backend.spawnApi();
+  beforeEach(async () => {
+    await NotificationHelper.clearAll();
+    await NotificationHelper.close();
+    user1 = await browser.backend.spawnUser();
+    user2 = await browser.backend.spawnUser();
+    group = GroupFactory.createGroupWithSpecificOwner(user1);
+    group.name = `BG notification test: ${utils.random.shortId()}`;
+    api1 = browser.backend.api;
+    api2 = await browser.backend.spawnApi();
 
-//         await api1.auth.login({
-//             identifier: user1.email,
-//             password: user1.password,
-//         });
-//         await api2.auth.login({
-//             identifier: user2.email,
-//             password: user2.password,
-//         });
+    await api1.auth.login({
+      identifier: user1.email,
+      password: user1.password,
+    });
+    await api2.auth.login({
+      identifier: user2.email,
+      password: user2.password,
+    });
 
-//         const groupResult = await api1.groups.createGroup({
-//             name: group.name,
-//         });
-//         group.id = groupResult.data.id;
-//         group.inviteCode = groupResult.data.inviteCode;
+    const groupResult = await api1.groups.createGroup({
+      name: group.name,
+    });
+    group.id = groupResult.data.id;
+    group.inviteCode = groupResult.data.inviteCode;
 
-//         await api2.groups.joinGroup({ code: group.inviteCode! });
+    await api2.groups.joinGroup({ code: group.inviteCode! });
 
-//         await loginScreen.openDirectly();
-//         await loginScreen.login({
-//             identifier: user2.email,
-//             password: user2.password,
-//         });
-//         await dashboardScreen.waitForIsShown();
-//         await NotificationHelper.closeApp();
-//     });
+    await loginScreen.openDirectly();
+    await loginScreen.login({
+      identifier: user2.email,
+      password: user2.password,
+    });
+    await dashboardScreen.waitForIsShown();
+    await NotificationHelper.closeApp();
+  });
 
-//     it('[HC-95] Verify push notification is received when a circle member changes status to "Потрібна допомога"', async () => {
-//         await api1.users.updateUserStatus({ status: 'DANGER' });
+  it('[HC-95] Verify push notification is received when a circle member changes status to "Потрібна допомога"', async () => {
+    await api1.users.updateUserStatus({ status: 'DANGER' });
 
-//         await NotificationHelper.open();
-//         const notification = await NotificationHelper.openAndTap(NotificationType.STATUS_UPDATE, {
-//             firstName: user1.firstName,
-//             lastName: user1.lastName,
-//         });
+    await NotificationHelper.open();
+    const notification = await NotificationHelper.openAndTap(NotificationType.STATUS_UPDATE, {
+      firstName: user1.firstName,
+      lastName: user1.lastName,
+    });
 
-//         await dashboardScreen.waitForIsShown();
+    await dashboardScreen.waitForIsShown();
 
-//         expect(notification).toBeDefined();
-//         expect(notification?.actualTitle).toBe('Оновлення статусу');
-//         expect(notification?.actualBody).toContain(user1.firstName);
-//         expect(notification?.actualBody).toContain(user1.lastName);
-//         expect(notification?.actualBody).toContain('треба допомога');
-//     });
+    expect(notification).toBeDefined();
+    expect(notification?.actualTitle).toBe('Оновлення статусу');
+    expect(notification?.actualBody).toContain(user1.firstName);
+    expect(notification?.actualBody).toContain(user1.lastName);
+    expect(notification?.actualBody).toContain('треба допомога');
+  });
 
-//     it('[HC-96] Verify push notification is received when a requested member updates their status', async () => {
-//         await api2.users.initiatePersonalRollCall(user1.id!);
+  it('[HC-96] Verify push notification is received when a requested member updates their status', async () => {
+    await api2.users.initiatePersonalRollCall(user1.id!);
 
-//         await api1.users.updateUserStatus({ status: 'SAFE' });
+    await api1.users.updateUserStatus({ status: 'SAFE' });
 
-//         await NotificationHelper.open();
-//         const notification = await NotificationHelper.openAndTap(NotificationType.STATUS_UPDATE, {
-//             firstName: user1.firstName,
-//             lastName: user1.lastName,
-//         });
+    await NotificationHelper.open();
+    const notification = await NotificationHelper.openAndTap(NotificationType.STATUS_UPDATE, {
+      firstName: user1.firstName,
+      lastName: user1.lastName,
+    });
 
-//         await dashboardScreen.waitForIsShown();
+    await dashboardScreen.waitForIsShown();
 
-//         expect(notification).toBeDefined();
-//         expect(notification?.actualTitle).toBe('Оновлення статусу');
-//         expect(notification?.actualBody).toContain(user1.firstName);
-//         expect(notification?.actualBody).toContain(user1.lastName);
-//         expect(notification?.actualBody).toContain('у безпеці');
-//     });
+    expect(notification).toBeDefined();
+    expect(notification?.actualTitle).toBe('Оновлення статусу');
+    expect(notification?.actualBody).toContain(user1.firstName);
+    expect(notification?.actualBody).toContain(user1.lastName);
+    expect(notification?.actualBody).toContain('у безпеці');
+  });
 
-//     it('[HC-97] Verify push notification delivery to all members during a Circle Roll Call', async () => {
-//         await api1.groups.initiateGroupRollCall(group.id!);
+  it('[HC-97] Verify push notification delivery to all members during a Circle Roll Call', async () => {
+    await api1.groups.initiateGroupRollCall(group.id!);
 
-//         await NotificationHelper.open();
-//         const notification = await NotificationHelper.openAndTap(NotificationType.ROLL_CALL, {
-//             groupName: group.name,
-//         });
+    await NotificationHelper.open();
+    const notification = await NotificationHelper.openAndTap(NotificationType.ROLL_CALL, {
+      groupName: group.name,
+    });
 
-//         await dashboardScreen.waitForIsShown();
+    await dashboardScreen.waitForIsShown();
 
-//         expect(notification).toBeDefined();
-//         expect(notification?.actualTitle).toContain('Перекличка!');
-//         expect(notification?.actualBody).toContain(group.name);
-//     });
+    expect(notification).toBeDefined();
+    expect(notification?.actualTitle).toContain('Перекличка!');
+    expect(notification?.actualBody).toContain(group.name);
+  });
 
-//     afterEach(async () => {
-//         await NotificationHelper.clearAll();
-//         await NotificationHelper.close();
-//     });
-// });
+  afterEach(async () => {
+    await NotificationHelper.clearAll();
+    await NotificationHelper.close();
+  });
+});
 
 describe('Background Air Alert Notifications', () => {
   let user: UserEntity;
+  let alertManager: UserEntity;
   let api: ApiClientFactory;
+  let region: AlertRegion;
 
   beforeEach(async () => {
     await NotificationHelper.clearAll();
     await NotificationHelper.close();
     user = await browser.backend.spawnUser();
-    api = browser.backend.api;
+    alertManager = await browser.backend.spawnUser();
+    api = await browser.backend.spawnApi();
+    region = utils.random.pickFromObject(AlertRegions)[0] as AlertRegion;
 
     await api.alertsMock.resetAll();
     await api.alerts.triggerSync();
@@ -128,7 +135,7 @@ describe('Background Air Alert Notifications', () => {
 
     await api.users.modifyUser({
       id: user.id!,
-      alertRegionUid: AlertRegions.KYIV_CITY.uid,
+      alertRegionUid: region.uid,
     });
 
     await loginScreen.openDirectly();
@@ -138,25 +145,28 @@ describe('Background Air Alert Notifications', () => {
     });
     await dashboardScreen.waitForIsShown();
     await NotificationHelper.closeApp();
+
+    await api.auth.login({
+      identifier: alertManager.email,
+      password: alertManager.password,
+    });
   });
 
   it('[HC-98] Verify critical push notification is received during an air raid alert in a subscribed region', async () => {
-    await api.alertsMock.startAlert(AlertRegions.KYIV_CITY);
+    await api.alertsMock.startAlert(region);
     await api.alerts.triggerSync();
-
-    await browser.pause(10_000);
 
     await NotificationHelper.open();
     const notification = await NotificationHelper.openAndTap(NotificationType.AIR_ALERT, {
-      regionName: AlertRegions.KYIV_CITY.title,
-      alertType: 'air_raid',
+      regionName: region.title,
+      alertType: 'Повітряна тривога',
     });
 
     await dashboardScreen.waitForIsShown();
 
     expect(notification).toBeDefined();
     expect(notification?.actualTitle).toContain('Повітряна тривога!');
-    expect(notification?.actualBody).toContain(AlertRegions.KYIV_CITY.title);
+    expect(notification?.actualBody).toContain(region.title);
   });
 
   afterEach(async () => {
